f2c88281e525d1adf10ce895d0e1960fae6a8239
Aris <frestaris@gmail.com>
2025-06-06 18:11:03 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Quiz from "./components/Quiz";
+import QuizSelector from "./components/QuizSelector";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Quiz />
+      <QuizSelector />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/QuizSelector.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuizSelector = () => {
+  return <div>QuizSelector</div>;
+};
+
+export default QuizSelector;
